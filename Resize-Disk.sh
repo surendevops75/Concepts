c48e1365db1bdef89d553e670dc0ff4f1990e365
surendevops75 <surendevops75@gmail.com>
2026-06-12 10:33:11 +0530

    Resize Disk

--- Resize-Disk.sh
+++ Resize-Disk.sh
@@ -1,32 +1,32 @@
 # Resize EBS Storage
 
-check the partitions
+#check the partitions
 ```
 lsblk
 ```
 
-Use a tool like growpart to resize the existing partition to fill the available space.This will resize the fourth partition (nvme0n1p4) to use the remaining unallocated space on the disk.
+#Use a tool like growpart to resize the existing partition to fill the available space.This will resize the fourth partition (nvme0n1p4) to use the remaining unallocated space on the disk.
 ```
 sudo growpart /dev/nvme0n1 4
 ```
 
-Extend the Logical Volumes
-Decide how much space to allocate to each logical volume. For example, to extend both the root and /var logical volumes:
+#Extend the Logical Volumes
+#Decide how much space to allocate to each logical volume. For example, to extend both the root and /var logical volumes:
 
 ```
 sudo lvextend -L +20G /dev/RootVG/rootVol
 sudo lvextend -L +30G /dev/RootVG/varVol
 ```
 
-After extending the logical volumes, resize the filesystems to utilize the additional space.
+#After extending the logical volumes, resize the filesystems to utilize the additional space.
 
-For the root filesystem:
+#For the root filesystem:
 
 ```
 sudo xfs_growfs /
 ```
 
-For the /var filesystem:
+#For the /var filesystem:
 
 ```
 sudo xfs_growfs /var
